742d17e2188f6a759b5e4705f120ece00759ec1e
Nick Rabone <n.rabone72@aol.com>
2024-06-08 16:12:42 +0100

    created features folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
 
   return (
       <div>
         <h1 className='text-7xl text-[#ff4500] font-bold bg-[#111] p-4'>E-Bazaar 2</h1>
+        <Button 
+          variant='destructive'
+          size='lg'
+          onClick={() => console.log('Button Pressed')}
+        >
+          Submit
+        </Button>
       </div>
   )
 }
