3b03bcea63696921da5545e6e562c23568702741
Nick Rabone <n.rabone72@aol.com>
2024-06-08 15:43:45 +0100

    Tailwind configured. Shadcn installed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
       <div>
-        <h1>E-Bazaar 2</h1>
+        <h1 className='text-7xl text-[#ff4500] font-bold bg-[#111] p-4'>E-Bazaar 2</h1>
       </div>
   )
 }
