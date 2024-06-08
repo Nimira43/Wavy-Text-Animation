5f3d74fec4b2a2149e651857950a71e69f421de1
Nick Rabone <n.rabone72@aol.com>
2024-06-08 16:27:37 +0100

    Redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button"
+import { useAppSelector } from "./hooks"
 
 function App() {
-
+  const {name} = useAppSelector((state) => state.userState)
+  console.log(name)
   return (
       <div>
         <h1 className='text-7xl text-[#ff4500] font-bold bg-[#111] p-4'>E-Bazaar 2</h1>
