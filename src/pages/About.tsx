3ff6fa4f571a43df782b0c98739879bcf87adfe9
Nimira43 <n.rabone72@aol.com>
2024-11-11 01:02:50 +0000

    About page completed.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,13 @@
 function About() {
-    return <h1 className='text-4xl'>About Page</h1>;
+    return (
+      <section>
+        <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
+          Explore and<span className='text-[#ff4500]'>Shop!</span>
+        </h1>
+        <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto">Discover stylish and affordable furniture at E-Bazaar. From cosy sofas to elegant dining sets, we offer a wide range of high-quality pieces to enhance any space. Shop now and transform your home with our exquisite collections</p>
+      </section>
+    )
+    
+    
   }
 export default About;
