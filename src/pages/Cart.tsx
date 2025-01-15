45bd967915405de85d5b6385200baf3664ac0516
Nimira43 <n.rabone72@aol.com>
2025-01-15 01:14:33 +0000

    Setup basic Cart page.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom'
+import { Button } from '../components/ui/button'
 function Cart() {
   return (
-    <h1 className='text-2xl'>Cart Page</h1>
+    <div>
+      <h1 className='text-3xl'>Cart Page</h1>
+      <Link to='/' className='text-2xl text-[#ff4500]'>
+        Back Home
+      </Link>
+      <Button asChild size='lg'>
+        <Link to='/'>Home</Link>
+      </Button>
+    </div>
   )
 }
 export default Cart
