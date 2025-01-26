28fa9ee4ec0591a800b269642593d0a4ed0d431e
Nimira43 <n.rabone72@aol.com>
2025-01-26 20:14:00 +0000

    BRought a lot of the files back into the product. Have commented out some lines of code. Rebuilding project slowly.

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,28 @@
+function Cart() {
+  return (
+    <div>
+      <h1>Cart</h1>
+    </div>
+  )
+}
+
+export default Cart
+
+
+
+// import { Link } from 'react-router-dom'
+// import { Button } from '../components/ui/button'
+// function Cart() {
+//   return (
+//     <div>
+//       <h1 className='text-3xl'>Cart Page</h1>
+//       <Link to='/' className='text-2xl text-[#ff4500]'>
+//         Back Home
+//       </Link>
+//       <Button asChild size='lg'>
+//         <Link to='/'>Home</Link>
+//       </Button>
+//     </div>
+//   )
+// }
+// export default Cart
---
+++ src/pages/HomeLayout.tsx
@@ -0,0 +1,24 @@
+function HomeLayout() {
+  return (
+    <div>
+      HomeLayout
+    </div>
+  )
+}
+
+export default HomeLayout
+
+
+
+// import { Outlet } from 'react-router-dom'
+
+// const HomeLayout = () => {
+//   return (
+//     <>
+//       <header>Header</header>
+//       <nav>Navbar</nav>
+//       <Outlet />
+//     </>
+//   )
+// }
+// export default HomeLayout
