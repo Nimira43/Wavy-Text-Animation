7a7e5092591ccb161ae8be0173d68aa5597be5ef
Nimira43 <n.rabone72@aol.com>
2025-01-30 19:59:16 +0000

    Testing Shadcn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages'
+
+import { Button } from './components/ui/button'
 import { useAppSelector } from './hooks'
+
 function App() {
   const { name } = useAppSelector((state) => state.userState);
   console.log(name)
+
+  return (
+    <div>
+      <h1 className='text-4xl font-bold '>E-Bazaar</h1>
+      <Button
+        size='lg'
+        onClick={() => console.log('Test Button')}
+      >
+        Submit
+      </Button>
+      <Cart />
+    </div>
+  )
 }
 
 export default App
 
 // import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-// import {
-//   HomeLayout,
-//   Landing,
-//   Error,
-//   Products,
-//   SingleProduct,
-//   Cart,
-//   About,
-//   Register,
-//   Login,
-//   Checkout,
-//   Orders,
-// } from './pages'
+
 
 
 // const router = createBrowserRouter([
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,5 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-// import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
 import { store } from './store'
@@ -18,6 +7,6 @@ import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <App   />
   </Provider>
 )
