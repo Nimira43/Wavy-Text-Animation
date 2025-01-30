5fc37899351aff14ce223d26aa67420ac04038c3
Nimira43 <n.rabone72@aol.com>
2025-01-30 20:08:14 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,80 +12,20 @@ import {
   Orders,
 } from './pages'
 
-import { Button } from './components/ui/button'
-import { useAppSelector } from './hooks'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-function App() {
-  const { name } = useAppSelector((state) => state.userState);
-  console.log(name)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+])
 
-  return (
-    <div>
-      <h1 className='text-4xl font-bold '>E-Bazaar</h1>
-      <Button
-        size='lg'
-        onClick={() => console.log('Test Button')}
-      >
-        Submit
-      </Button>
-      <Cart />
-    </div>
-  )
+const App = () => {
+  return <RouterProvider router={router} />
 }
-
 export default App
-
-// import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
-
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <HomeLayout />,
-
-//     children: [
-//       {
-//         index: true,
-//         element: <Landing />,
-//       },
-//       {
-//         path: 'products',
-//         element: <Products />,
-//       },
-//       {
-//         path: 'products/:id',
-//         element: <SingleProduct />,
-//       },
-//       {
-//         path: 'cart',
-//         element: <Cart />,
-//       },
-//       { 
-//         path: 'about', 
-//         element: <About /> },
-//       {
-//         path: 'checkout',
-//         element: <Checkout />,
-//       },
-//       {
-//         path: 'orders',
-//         element: <Orders />,
-//       },
-//     ],
-//   },
-//   {
-//     path: '/login',
-//     element: <Login />,
-//   },
-//   {
-//     path: '/register',
-//     element: <Register />,
-//   },
-// ])
-
-// const App = () => {
-//   return <RouterProvider router={router} />;
-// }
-// export default App
