11b035a3f87765d730f0501a099beed796593667
Nimira43 <n.rabone72@aol.com>
2025-01-30 20:34:58 +0000

    Completed Header component for now including conditional rendering (user signed in or not), logout functionality and sign in / register links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button } from './ui/button'
+import { useState } from 'react'
 
 const Header = () => {
   const navigate = useNavigate()
+
   const [user, setUser] = useState<{ username: string } | null>({
     username: 'Test User',
   })
@@ -12,9 +15,27 @@ const Header = () => {
   }
 
   return (
-    <div>
-      
-    </div>
+    <header>
+      <div className='align-items flex justify-center sm:justify-end py-2'>
+        {user ? (
+          <div className='flex gap-x-2 sm:gap-x-8 items-center'>
+            <p className='text-xs sm:text-sm'>Hi, {user.username}</p>
+            <Button size='sm' onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <div className='flex gap-x-6 justify-center items-center -mr-4'>
+            <Button asChild variant='link' size='sm'>
+              <Link to='/login'>Sign In</Link>
+            </Button> 
+            <Button asChild variant='link' size='sm'>
+              <Link to='/register'>Register</Link>
+            </Button> 
+          </div>
+        )}
+      </div> 
+    </header>
   )
 }
 
