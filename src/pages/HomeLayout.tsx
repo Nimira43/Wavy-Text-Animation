96c46b1c1e46d4d858315f2370af800021459faa
Nimira43 <n.rabone72@aol.com>
2025-01-30 20:36:18 +0000

    Brought Header into App.tsx.

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import { Header } from '../components'
 
 const HomeLayout = () => {
   return (
     <>
-      <header>Header</header>
+      <Header />
       <nav>Navbar</nav>
       <Outlet />
     </>
