87e4b07b1e5fe2b2afe8b9c05b7a613f151ec590
Nimira43 <n.rabone72@aol.com>
2025-02-05 21:16:27 +0000

    LinksDropdown updated.

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -1,9 +1,45 @@
-const LinksDropdown = () => {
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../components/ui/dropdown-menu'
+import { PiAlignLeft } from 'react-icons/pi'
+import { Button } from './ui/button'
+import { links } from './utils/links'
+import { NavLink } from 'react-router-dom'
+
+function LinksDropdown() {
   return (
-    <div>
-      Links Dropdown
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button variant='outline' size='icon'>
+          <PiAlignLeft />
+
+          <span className='sr-only'>Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-52 lg:hidden '
+        align='start'
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.label}>
+              <NavLink
+                to={link.href}
+                className={({ isActive }) => {
+                  return `capitalize w-full ${isActive ? 'text-primary' : ''}`
+                }}
+              >
+                {link.label}
+              </NavLink>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
-
 export default LinksDropdown
