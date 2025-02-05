a17e6088c5010045a1d30a3ba6995e5daa2be26c
Nimira43 <n.rabone72@aol.com>
2025-02-05 20:56:32 +0000

    Developed Navbar component. For now added atemporary skyblue colour that will change at some point

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,22 @@
+import CartButton from './CartButton'
+import LinksDropdown from './LinksDropdown'
+import Logo from './Logo'
+import ModeToggle from './ModeToggle'
+import NavLinks from './NavLinks'
+
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='bg-[#87ceeb] py-4'>
+      <div className='align-element flex justify-between items-center'>
+        <Logo />
+        <LinksDropdown />
+        <NavLinks />
+        <div className='flex justify-center items-center gap-x-4'>
+          <ModeToggle />
+          <CartButton />
+        </div>
+      </div>
+    </nav>
   )
 }
 
