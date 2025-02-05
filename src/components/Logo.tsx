6354e99076dd25f0a00499b1d91f428f7dae40f8
Nimira43 <n.rabone72@aol.com>
2025-02-05 21:03:26 +0000

    Logo icon brought in. React Icons brought in.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,15 @@
-const Logo = () => {
+import { Link } from 'react-router-dom'
+import { TfiLayoutAccordionList } from 'react-icons/tfi'
+
+function Logo() {
   return (
-    <div>
-      Logo
-    </div>
-  )
+    <Link
+      to='/'
+      className='hidden lg:flex justify-center items-center bg-primary p-2 rounded-lg text-[#fffcfa] '
+    >
+      <TfiLayoutAccordionList className='w-8 h-8' />
+    </Link>
+  );
 }
 
-export default Logo
+export default Logo;
