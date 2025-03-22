6354b7dbd7fea5cbae672190d3dc21064dce4512
Nimira43 <n.rabone72@aol.com>
2025-03-22 20:39:49 +0000

    Forgot to return the 404 message content - now corrected. Also added this error component into the routes setup inside App.tsx

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -5,30 +5,32 @@ function Error() {
   const error = useRouteError()
   console.log(error)
   if (isRouteErrorResponse(error) && error.status === 404) {
-    <main className='grid min-h-[100vh] place-items-center px-8'>
-      <div className='text-center'>
-        <p className='text-9xl font-semibold text-primary'>404</p>
-        <h1
-          className='mt-4 text-3xl font-medium tracking-tight sm:text-5xl'
-        >
-          Page Not Found
-        </h1>
-        <p className='mt-6 text-lg leading-7'>We cannot find the page that you are looking for.</p>
-      </div>
-      <div className='mt-10'>
-        <Button
-          asChild 
-          size='lg'  
-        >
-          <Link
-            to='/'
-            className='uppercase'
+    return (
+      <main className='grid min-h-[100vh] place-items-center px-8'>
+        <div className='text-center'>
+          <p className='text-9xl font-semibold text-primary'>404</p>
+          <h1
+            className='mt-4 text-3xl font-medium tracking-tight sm:text-5xl'
           >
-            Home
-          </Link>
-        </Button>
-      </div>
-    </main>
+            Page Not Found
+          </h1>
+          <p className='mt-6 text-lg leading-7'>We cannot find the page that you are looking for.</p>
+        </div>
+        <div className='mt-10'>
+          <Button
+            asChild 
+            size='lg'  
+          >
+            <Link
+              to='/'
+              className='uppercase'
+            >
+              Home
+            </Link>
+          </Button>
+        </div>
+      </main>
+    )
   }
   return (
     <main className='grid min-h-[100vh] place-items-center px-8'>
