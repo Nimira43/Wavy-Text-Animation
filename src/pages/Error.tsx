6cc57b71dcdd58020576bc8f3f0bc8679daa6443
Nimira43 <n.rabone72@aol.com>
2025-03-22 20:10:02 +0000

    Working on Error.tsx - brought in imports

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,3 +1,6 @@
+import { useRouteError, Link, isRouteErrorResponse } from 'react-router-dom'
+import { Button } from '../components/ui/button'
+
 function Error() {
   return (
     <h1 className='text-2xl'>Error Page</h1>
