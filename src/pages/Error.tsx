d1717e3eaa3b00b63d7963625f8bf68bd2e2482a
Nimira43 <n.rabone72@aol.com>
2025-03-22 20:20:35 +0000

    setting up useRouteError hook and isRouteErrorResponseutility function

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -2,8 +2,11 @@ import { useRouteError, Link, isRouteErrorResponse } from 'react-router-dom'
 import { Button } from '../components/ui/button'
 
 function Error() {
-  return (
-    <h1 className='text-2xl'>Error Page</h1>
-  )
+  const error = useRouteError()
+  console.log(error)
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    <h1>404 Error</h1>
+  }
+  return <h1 className='text-2xl'>Error Page</h1>
 }
 export default Error
