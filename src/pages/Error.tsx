856aa1a0e61ee72a48fe0cbd88b3997fe9ee0df4
Nimira43 <n.rabone72@aol.com>
2025-03-22 20:27:54 +0000

    Setting up on screen error message

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -5,7 +5,18 @@ function Error() {
   const error = useRouteError()
   console.log(error)
   if (isRouteErrorResponse(error) && error.status === 404) {
-    <h1>404 Error</h1>
+    <main className='grid min-h-[100vh] place-items-center px-8'>
+      <div className='text-center'>
+        <p className='text-9xl font-semibold text-primary'>404</p>
+        <h1
+          className='mt-4 text-3xl font-medium tracking-tight sm:text-5xl'
+        >
+          Page Not Found
+        </h1>
+        <p className='mt-6 text-lg leading-7'>We cannot find the page that you are looking for.</p>
+      </div>
+      <div></div>
+    </main>
   }
   return <h1 className='text-2xl'>Error Page</h1>
 }
