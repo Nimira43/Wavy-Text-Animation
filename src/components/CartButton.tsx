50a3bd4e5cd8cc2474731a57e9c46b05ca9ffce6
Nimira43 <n.rabone72@aol.com>
2025-03-22 20:07:12 +0000

    Styled CartButton

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,8 +1,26 @@
-const CartButton = () => {
+import { Link } from 'react-router-dom'
+import { Button } from './ui/button'
+import { IoCartOutline } from 'react-icons/io5'
+
+function CartButton() {
+  const numItemsInCart = 2
+  
   return (
-    <div>
-      CartButton
-    </div>
+      <Button
+        asChild
+        variant='outline'
+        size='icon'
+        className='flex justify-center items-center relative'
+      >
+        <Link to='/cart'>
+            <IoCartOutline />
+            <span
+              className='absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs' 
+            >
+              {numItemsInCart}
+            </span>
+        </Link>
+      </Button>    
   )
 }
 
