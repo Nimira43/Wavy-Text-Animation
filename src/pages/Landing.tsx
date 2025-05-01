cfdc96a374f526cd322005d97ad9d65b578ffdd4
Nimira43 <n.rabone72@aol.com>
2025-05-01 16:28:20 +0100

    Exporting loader function

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,6 +4,11 @@ import { useLoaderData, type LoaderFunction } from 'react-router-dom'
 
 const url = '/products?featured=true'
 
+export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
+  const response = await customFetch<ProductsResponse>(url)
+  return { ...response.data}
+}
+
 function Landing() {
   return (
     <>
