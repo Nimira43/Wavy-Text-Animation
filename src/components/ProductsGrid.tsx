35e05e99bfd95d83e7e5810f0aa6d6527ccf727f
Nimira43 <n.rabone72@aol.com>
2025-05-01 18:54:28 +0100

    ProductsGrid - 2

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -1,11 +1,22 @@
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 import { ProductsResponse } from '../utils'
 
 const ProductsGrid = () => {
   const { data: products} = useLoaderData as ProductsResponse
   
   return (
-    <div>
+    <div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      {products.map((product) => {
+        const { title, price, image } = product.attributes
+        const poundsAmount = formatAsPounds(price)
+
+        return (
+          <Link to={`/products/${product.id}`} key={product.id}>
+            
+          </Link>
+          
+        )
+      })}
       
     </div>
   )
