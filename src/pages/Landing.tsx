f6327770e74231efec1dbacfa08c42c160293a7c
Nimira43 <n.rabone72@aol.com>
2025-05-01 16:23:40 +0100

    Imports and url const.

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,8 @@
 import { Hero, FeaturedProducts } from '../components'
+import { customFetch, type ProductsResponse } from '../utils'
+import { useLoaderData, type LoaderFunction } from 'react-router-dom'
+
+const url = '/products?featured=true'
 
 function Landing() {
   return (
