ab1df1946c479a90b656249bb549b34bf31a4c70
Nimira43 <n.rabone72@aol.com>
2025-05-01 18:22:29 +0100

    Import loader as landingLoader in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import {
   Orders,
 } from './pages'
 import { ErrorElement } from './components'
+import { loader as landingLoader } from './pages/Landing'
 
 const router = createBrowserRouter([
   {
