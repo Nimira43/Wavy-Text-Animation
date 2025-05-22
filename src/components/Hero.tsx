e50bf3fcae9db96c11f1773f70c62d1e8826e3bf
Nimira43 <n.rabone72@aol.com>
2025-05-22 23:23:37 +0100

    Hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,12 @@ import HeroCarousel from './HeroCarousel'
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
-    </div>
+    <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
+      <div>
+        <h1 className='max-w-2xl text-4xl font-medium tracking-tight sm:text-6xl'>Redefining The Ways You Shop</h1>
+        <p className='mt-8 max-w-xl text-lg leading-8'>Discover furniture that transforms your space. At e-Bazaar, style meets comfort with expertly crafted pieces designed for modern living. Elevate your home effortlessly — shop now for quality, affordability, and timeless elegance. Your dream space awaits</p>
+      </div>
+    </section>
   )
 }
 
