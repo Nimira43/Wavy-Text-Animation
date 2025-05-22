f13546bcf0c73113dc9a75ff9861c42e3e295e3d
Nimira43 <n.rabone72@aol.com>
2025-05-22 23:28:24 +0100

    Added button link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,6 +8,13 @@ const Hero = () => {
       <div>
         <h1 className='max-w-2xl text-4xl font-medium tracking-tight sm:text-6xl'>Redefining The Ways You Shop</h1>
         <p className='mt-8 max-w-xl text-lg leading-8'>Discover furniture that transforms your space. At e-Bazaar, style meets comfort with expertly crafted pieces designed for modern living. Elevate your home effortlessly — shop now for quality, affordability, and timeless elegance. Your dream space awaits</p>
+        <Button 
+          asChild
+          size='lg'
+          className='mt-10 uppercase'
+        >
+          <Link to='./products'>Our Products</Link>
+        </Button>
       </div>
       <HeroCarousel />
     </section>
