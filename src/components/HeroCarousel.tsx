37fa515c2365b5b96eafd24314a8d9f89f59c3a3
Nimira43 <n.rabone72@aol.com>
2025-05-31 16:21:13 +0100

    Card content image.

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -2,6 +2,7 @@ import hero1 from '../assets/hero1.webp'
 import hero2 from '../assets/hero2.webp'
 import hero3 from '../assets/hero3.webp'
 import hero4 from '../assets/hero4.webp'
+import { Card, CardContent } from './ui/card'
 import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
 
 const carouselImages = [hero1, hero2, hero3, hero4]
@@ -12,7 +13,17 @@ function HeroCarousel() {
       <Carousel>
         <CarouselContent>
           {carouselImages.map((image, index) => (
-            <CarouselItem key={index}
+            <CarouselItem key={index}>
+              <Card>
+                <CardContent className='p-2'>
+                  <img 
+                    className='w-full h-[24rem] rounded-md object-cover'
+                    src={image} 
+                    alt='hero'
+                  />
+                </CardContent>
+              </Card>
+            </CarouselItem>
           ))}
         </CarouselContent>
       </Carousel>
