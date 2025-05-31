c9f79042c77afaa93bcd260f4d90faf902a93ca3
Nimira43 <n.rabone72@aol.com>
2025-05-31 16:35:34 +0100

    Imports for ProductsList

---
+++ src/components/ProductsList.tsx
@@ -0,0 +1,13 @@
+import { Filters, ProductsContainer, PaginationContainer } from '../components'
+import { customFetch, type ProductsResponse }  from '../utils'
+import { type LoaderFunction } from 'react-router-dom'
+
+const ProductsList = () => {
+  return (
+    <div>
+      <h1>Products List</h1>      
+    </div>
+  )
+}
+
+export default ProductsList
