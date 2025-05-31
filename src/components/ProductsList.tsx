5b021a35c5440ba8aedf8a9dbae52691fa64cc06
Nimira43 <n.rabone72@aol.com>
2025-05-31 17:05:24 +0100

    Setting up link and card

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,5 +1,6 @@
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 import { formatAsPounds, type ProductsResponse } from '../utils'
+import { Card, CardContent } from './ui/card'
 
 const ProductsList = () => {
   const { data: products } = useLoaderData() as ProductsResponse
@@ -9,6 +10,20 @@ const ProductsList = () => {
       {products.map((product) => {
         const { title, price, image, company} = product.attributes
         const poundsAmount = formatAsPounds(price)
+
+        return (
+          <Link 
+            key={product.id}
+            to={`/products/${product.id}`}
+          >
+            <Card>
+              <CardContent className='p-8 gap-y-4 grid md:grid-cols-3'>
+                
+              </CardContent>
+            </Card>
+
+          </Link> 
+        )
       })}      
     </div>
   )
