8aa72b5a7927d4bc20a34f628b270081ceb47e20
Nimira43 <n.rabone72@aol.com>
2025-05-31 16:48:00 +0100

    Updated ProductsList.tsx

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,15 +1,10 @@
-// import { Filters, ProductsContainer, PaginationContainer } from '../components'
-import { customFetch, type ProductsResponse }  from '../utils'
-import { type LoaderFunction } from 'react-router-dom'
-
 const ProductsList = () => {
   return (
-    <>
-      <Filters />
-      <ProductsContainer />
-      <PaginationContainer />
-    </>
+    <div>
+      <h1>Products List</h1>      
+    </div>
   )
 }
 
 export default ProductsList
+
