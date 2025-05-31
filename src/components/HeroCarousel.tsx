01fa4d5fbe862d0d4ae68713927126e1aba50901
Nimira43 <n.rabone72@aol.com>
2025-05-31 09:10:33 +0100

    Importing images and placing them inside carouselImages array

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -1,3 +1,10 @@
+import hero1 from '../assets/hero1.webp'
+import hero2 from '../assets/hero2.webp'
+import hero3 from '../assets/hero3.webp'
+import hero4 from '../assets/hero4.webp'
+
+const carouselImages = [hero1, hero2, hero3, hero4]
+
 function HeroCarousel() {
   return (
     <div>
