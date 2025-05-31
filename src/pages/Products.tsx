c7e9b2a9397b1c899a3bf707076bf69f9bfae575
Nimira43 <n.rabone72@aol.com>
2025-05-31 16:53:14 +0100

    Set up loader

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,6 +4,13 @@ import ProductsContainer from '../components/ProductsContainer'
 import { customFetch, type ProductsResponse }  from '../utils'
 import { type LoaderFunction } from 'react-router-dom'
 
+const url = '/products'
+
+export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
+  const response = await customFetch<ProductsResponse>(url)
+  return { ...response.data }
+}
+
 const ProductsList = () => {
   return (
     <>
