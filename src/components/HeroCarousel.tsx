ebd66b5a148db433bb8316bbb6474a04af903ee6
Nimira43 <n.rabone72@aol.com>
2025-05-31 09:15:26 +0100

    Starting to map through carousel items.

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -2,13 +2,20 @@ import hero1 from '../assets/hero1.webp'
 import hero2 from '../assets/hero2.webp'
 import hero3 from '../assets/hero3.webp'
 import hero4 from '../assets/hero4.webp'
+import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
 
 const carouselImages = [hero1, hero2, hero3, hero4]
 
 function HeroCarousel() {
   return (
-    <div>
-      Hero Carousel
+    <div className='hidden bg:block'>
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, index) => (
+            <CarouselItem key={index}
+          ))}
+        </CarouselContent>
+      </Carousel>
     </div>
   )
 }
