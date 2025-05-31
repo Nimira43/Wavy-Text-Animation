1d82547da569a7aacb66be6687af3b35d3ead31b
Nimira43 <n.rabone72@aol.com>
2025-05-31 20:40:36 +0100

    Setting up form, label and input.

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,8 +1,20 @@
 import { Form } from 'react-router-dom'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
 
 const Filters = () => {
   return (
-    <Form></Form>
+    <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+      <div className='mb-2'>
+        <Label htmlFor='search'>Search Product</Label> 
+        <Input
+          id='search'
+          name='search'
+          type='text'
+          defaultValue=''
+        />
+      </div>
+    </Form>
   )
 }
 
