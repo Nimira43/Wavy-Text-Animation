d23d1bc3707bb2dd1da842dabee4fb6c4b6680fb
Nimira43 <n.rabone72@aol.com>
2025-05-31 17:01:22 +0100

    Starting to map products.

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -5,8 +5,11 @@ const ProductsList = () => {
   const { data: products } = useLoaderData() as ProductsResponse
 
   return (
-    <div>
-      <h1>Products List</h1>      
+    <div className='mt-12 grid gap-y-8'>
+      {products.map((product) => {
+        const { title, price, image, company} = product.attributes
+        const poundsAmount = formatAsPounds(price)
+      })}      
     </div>
   )
 }
