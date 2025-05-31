595bfedbdae9b5f70e7ee98f38f7a7bf25154f1d
Nimira43 <n.rabone72@aol.com>
2025-05-31 16:57:47 +0100

    Importing formatAsPounds

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,4 +1,9 @@
+import { useLoaderData } from 'react-router-dom'
+import { formatAsPounds, type ProductsResponse } from '../utils'
+
 const ProductsList = () => {
+  const { data: products } = useLoaderData() as ProductsResponse
+
   return (
     <div>
       <h1>Products List</h1>      
