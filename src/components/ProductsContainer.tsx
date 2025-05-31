4d29405a4def998ac70aee31b576811b6dd62640
Nimira43 <n.rabone72@aol.com>
2025-05-31 19:04:19 +0100

    Setting up variables inside ProductsContainer

--- src/components/ProductsContainer.tsx
+++ src/components/ProductsContainer.tsx
@@ -1,4 +1,12 @@
+import { useLoaderData } from 'react-router-dom'
+import { ProductsResponse } from '../utils'
+import { useState } from 'react'
+
 const ProductsContainer = () => {
+  const { meta } = useLoaderData() as ProductsResponse
+  const totalProducts = meta.pagination.total
+  const [layout, setLayout] = useState<'grid | 'list>
+  
   return (
     <div>
       <h1>Products Container</h1>      
