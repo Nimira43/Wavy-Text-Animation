809b230840e18a31dd6fd423f5493133c7726b59
Nimira43 <n.rabone72@aol.com>
2025-05-31 16:25:13 +0100

    Corrected typo on line 12 of HeroCarousel.

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -3,13 +3,13 @@ import hero2 from '../assets/hero2.webp'
 import hero3 from '../assets/hero3.webp'
 import hero4 from '../assets/hero4.webp'
 import { Card, CardContent } from './ui/card'
-import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel'
 
 const carouselImages = [hero1, hero2, hero3, hero4]
 
 function HeroCarousel() {
   return (
-    <div className='hidden bg:block'>
+    <div className='hidden lg:block'>
       <Carousel>
         <CarouselContent>
           {carouselImages.map((image, index) => (
@@ -26,6 +26,8 @@ function HeroCarousel() {
             </CarouselItem>
           ))}
         </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
       </Carousel>
     </div>
   )
