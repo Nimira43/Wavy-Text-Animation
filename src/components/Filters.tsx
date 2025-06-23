9fc91de24a1eccd03d75c969ad5168871cd8c285
Nimira43 <n.rabone72@aol.com>
2025-06-23 23:40:38 +0100

    Updated

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,9 +1,13 @@
-import { Form, Link } from 'react-router-dom'
+import { Form, Link, useLoaderData } from 'react-router-dom'
 import { Label } from './ui/label'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
+import { ProductsResponseWithParams } from '../utils'
 
 const Filters = () => {
+  const {meta, params} = useLoaderData() as ProductsResponseWithParams
+  const { search } = params
+
   return (
     <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
       <div className='mb-2'>
@@ -12,7 +16,7 @@ const Filters = () => {
           id='search'
           name='search'
           type='text'
-          defaultValue=''
+          defaultValue={search}
         />
       </div>
       <Button
